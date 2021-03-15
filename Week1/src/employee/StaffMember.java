9e54e638285fa313656abb4eba40324708c2380c
mrizkymaulidhan <mrizky04@gmail.com>
2021-03-15 11:32:11 +0700

    Menuliskan source code.
    Menuliskan semua source code yang ada dalam persoalan.

---
+++ Week1/src/employee/StaffMember.java
@@ -0,0 +1,40 @@
+package employee;
+
+//********************************************************************
+//  StaffMember.java       Author: Lewis/Loftus
+//
+//  Represents a generic staff member.
+//********************************************************************
+
+abstract public class StaffMember
+{
+   protected String name;
+   protected String address;
+   protected String phone;
+
+   //-----------------------------------------------------------------
+   //  Sets up this staff member using the specified
+   //  information.
+   //-----------------------------------------------------------------
+   public StaffMember(String eName, String eAddress, String ePhone)
+   {
+      name = eName;
+      address = eAddress;
+      phone = ePhone;
+   }
+
+   //-----------------------------------------------------------------
+   //  Returns a string including the basic employee information.
+   //-----------------------------------------------------------------
+   public String toString()
+   {
+      String result = "Name: " + name + "\n";
+
+      result += "Address: " + address + "\n";
+      result += "Phone: " + phone;
+
+      return result;
+   }
+
+   public abstract double pay();
+}

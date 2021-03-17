dca8c65acfe8660cc6b4333a071969f56dfefbac
mrizkymaulidhan <mrizky04@gmail.com>
2021-03-17 17:30:52 +0700

    Merapihkan penulisan kode.
    Merapihkan penulisan kode.

--- Week-1/PaintingShapes/src/paintingshapes/Cylinder.java
+++ Week-1/PaintingShapes/src/paintingshapes/Cylinder.java
@@ -3,21 +3,25 @@ package paintingshapes;
 //***************************************************************************
 //Cylinder.java      Author: M.Rizky
 //***************************************************************************
-public class Cylinder extends Shape { 
+public class Cylinder extends Shape 
+{ 
     private double radius;  
     private double height;  
   
-    public Cylinder(double r, double d) {
+    public Cylinder(double r, double d) 
+    {
         super("Cylinder"); 
         this.radius = r;
         this.height = d;
     } 
     
-    public double area() {
+    public double area() 
+    {
         return Math.PI * radius * radius * height;
     }
   
-    public String toString() {
+    public String toString() 
+    {
         return super.toString() + " of radius " + radius + " and height " + height;
     }
 }
